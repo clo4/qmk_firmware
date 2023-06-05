a283cc69c6f30d85bf475f5ebc3dfe844417676e
Robert Clover <robert@clover.gdn>
2023-06-05 13:49:50 +1000

    More styleguide

--- keyboards/ferris/keymaps/clo4/keymap.c
+++ keyboards/ferris/keymaps/clo4/keymap.c
@@ -4,7 +4,7 @@ enum layers {
   _COLEMAK_DH,
   _EXT,
   _SYM,
-  _FN,
+  _FUN,
   _NUM,
   _RES,
 };
@@ -25,6 +25,21 @@ enum layers {
 //
 // Seniply is a well-designed layout that pairs extremely well with Colemak-DH.
 // It was created by the same person that created Colemak-DH.
+//
+// The only major change from the stock layout is how the layers are ordered.
+// There are 4 thumb keys on the Ferris Sweep: A, B # C, D (where # is the split)
+// B is shift and C is space. These never change in any layer.
+// Hold A to enter EXT. Hold A->D to enter FUN.
+// Hold D to enter SYM. Hold D->A to enter NUM.
+//
+// There is also a RES layer to type QK_BOOT. This is accessed by entering the 
+// NUM layer, holding the top-right key, then tapping the key in the W position.
+
+// Styleguide:
+// - Limit key names to at most 8 chars
+// - Align by columns (see below)
+// - Keep two spaces either side of the split comment /**/
+// - Use XXXXXXX and _______ instead of the corresponding KC_*
 
 // To align by cols, type this sequence when your cursor is over the { below
 // mi{sLAY<enter>j3xS,_<A-;>&
@@ -37,6 +52,7 @@ enum layers {
 // - _ trims any whitespace from the selections
 // - <A-;> swaps the selection head and anchor, so the alignment will happen on the left
 // - & aligns the selections to columns
+// You must align the thumb row manually.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_COLEMAK_DH] = LAYOUT(
     KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,     /**/  KC_J,    KC_L,    KC_U,    KC_Y,    KC_QUOT,
@@ -48,30 +64,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  /**/  KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_CAPS,
     OSM_LALT, OSM_LGUI, OSM_LSFT, OSM_LCTL, OSM_RALT, /**/  KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_DEL,
     KC_LCTZ,  KC_LCTX,  KC_LCTC,  KC_LGUI,  KC_LCTV,  /**/  KC_ENT,  KC_BSPC, KC_TAB,  KC_APP,  KC_PSCR,
-                                  KC_TRNS,  XXXXXXX,  /**/  KC_ENT,  MO(_FN) 
+                                  _______,  XXXXXXX,  /**/  KC_ENT,  MO(_FUN) 
   ),
   [_SYM] = LAYOUT(
     KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  /**/  KC_EQL,  KC_GRV,  KC_COLN, KC_SCLN, KC_PLUS,
     KC_LALT,  KC_LGUI,  KC_LSFT,  KC_LCTL,  KC_CIRC,  /**/  KC_ASTR, KC_LPRN, KC_LCBR, KC_LBRC, KC_MINS,
     XXXXXXX,  XXXXXXX,  KC_BSLS,  KC_PIPE,  KC_AMPR,  /**/  KC_TILD, KC_RPRN, KC_RCBR, KC_RBRC, KC_UNDS,
-                                  MO(_FN),  MO(_NUM), /**/  XXXXXXX, KC_TRNS
+                                  MO(_NUM), XXXXXXX,  /**/  XXXXXXX, _______
   ),
-  [_FN] = LAYOUT(
-    KC_MSTP,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_BRIU,  /**/  KC_F12,  KC_F7,   KC_F8,   KC_F9,   KC_NO,
-    KC_LALT,  KC_LGUI,  KC_LSFT,  KC_LCTL,  KC_BRID,  /**/  KC_F11,  KC_F4,   KC_F5,   KC_F6,   KC_NO,
-    KC_MUTE,  KC_VOLD,  XXXXXXX,  KC_VOLU,  XXXXXXX,  /**/  KC_F10,  KC_F1,   KC_F2,   KC_F3,   KC_NO,
-                                  KC_TRNS,  XXXXXXX,  /**/  XXXXXXX, KC_TRNS
+  [_FUN] = LAYOUT(
+    KC_MSTP,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_BRIU,  /**/  KC_F12,  KC_F7,   KC_F8,   KC_F9,   XXXXXXX,
+    KC_LALT,  KC_LGUI,  KC_LSFT,  KC_LCTL,  KC_BRID,  /**/  KC_F11,  KC_F4,   KC_F5,   KC_F6,   XXXXXXX,
+    KC_MUTE,  KC_VOLD,  XXXXXXX,  KC_VOLU,  XXXXXXX,  /**/  KC_F10,  KC_F1,   KC_F2,   KC_F3,   XXXXXXX,
+                                  _______,  XXXXXXX,  /**/  XXXXXXX, _______
   ),
   [_NUM] = LAYOUT(
     MO(_RES), XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  /**/  KC_PEQL, KC_7,    KC_8,    KC_9,    KC_PPLS,
     KC_LALT,  KC_LGUI,  KC_LSFT,  KC_LCTL,  KC_RALT,  /**/  KC_PAST, KC_4,    KC_5,    KC_6,    KC_PMNS,
     XXXXXXX,  KC_APP,   KC_TAB,   KC_BSPC,  KC_ENT,   /**/  KC_0,    KC_1,    KC_2,    KC_3,    KC_PSLS,
-                                  XXXXXXX,  KC_TRNS,  /**/  XXXXXXX, KC_NO
+                                  _______,  XXXXXXX,  /**/  XXXXXXX, _______
   ),
   [_RES] = LAYOUT(
-    KC_TRNS,  QK_BOOT,  XXXXXXX,  XXXXXXX,  XXXXXXX,  /**/  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_NO,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  /**/  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_NO,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  /**/  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_NO,
-                                  XXXXXXX,  XXXXXXX,  /**/  XXXXXXX, KC_NO 
+    _______,  QK_BOOT,  XXXXXXX,  XXXXXXX,  XXXXXXX,  /**/  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  /**/  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  /**/  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                                  XXXXXXX,  XXXXXXX,  /**/  XXXXXXX, XXXXXXX 
   ),
 };
