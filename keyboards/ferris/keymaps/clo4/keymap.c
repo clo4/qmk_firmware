590d841277026cf6e4553e1ce02530bf995b8924
Robert Clover <robert@clover.gdn>
2023-06-05 12:40:37 +1000

    Use XXXXXXX instead of KC_NO
    This change makes it easier to see unassigned keys

--- keyboards/ferris/keymaps/clo4/keymap.c
+++ keyboards/ferris/keymaps/clo4/keymap.c
@@ -45,33 +45,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                   MO(_EXT), KC_LSFT,  /**/  KC_SPC,  MO(_SYM)
   ),
   [_EXT] = LAYOUT(
-    KC_ESC,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    /**/  KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_CAPS,
+    KC_ESC,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  /**/  KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_CAPS,
     OSM_LALT, OSM_LGUI, OSM_LSFT, OSM_LCTL, OSM_RALT, /**/  KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_DEL,
     KC_LCTZ,  KC_LCTX,  KC_LCTC,  KC_LGUI,  KC_LCTV,  /**/  KC_ENT,  KC_BSPC, KC_TAB,  KC_APP,  KC_PSCR,
-                                  KC_TRNS,  KC_NO,    /**/  KC_ENT,  MO(_FN) 
+                                  KC_TRNS,  XXXXXXX,  /**/  KC_ENT,  MO(_FN) 
   ),
   [_SYM] = LAYOUT(
     KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  /**/  KC_EQL,  KC_GRV,  KC_COLN, KC_SCLN, KC_PLUS,
     KC_LALT,  KC_LGUI,  KC_LSFT,  KC_LCTL,  KC_CIRC,  /**/  KC_ASTR, KC_LPRN, KC_LCBR, KC_LBRC, KC_MINS,
-    KC_NO,    KC_NO,    KC_BSLS,  KC_PIPE,  KC_AMPR,  /**/  KC_TILD, KC_RPRN, KC_RCBR, KC_RBRC, KC_UNDS,
-                                  MO(_FN),  MO(_NUM), /**/  KC_NO,   KC_TRNS
+    XXXXXXX,  XXXXXXX,  KC_BSLS,  KC_PIPE,  KC_AMPR,  /**/  KC_TILD, KC_RPRN, KC_RCBR, KC_RBRC, KC_UNDS,
+                                  MO(_FN),  MO(_NUM), /**/  XXXXXXX, KC_TRNS
   ),
   [_FN] = LAYOUT(
     KC_MSTP,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_BRIU,  /**/  KC_F12,  KC_F7,   KC_F8,   KC_F9,   KC_NO,
     KC_LALT,  KC_LGUI,  KC_LSFT,  KC_LCTL,  KC_BRID,  /**/  KC_F11,  KC_F4,   KC_F5,   KC_F6,   KC_NO,
-    KC_MUTE,  KC_VOLD,  KC_NO,    KC_VOLU,  KC_NO,    /**/  KC_F10,  KC_F1,   KC_F2,   KC_F3,   KC_NO,
-                                  KC_TRNS,  KC_NO,    /**/  KC_NO,   KC_TRNS
+    KC_MUTE,  KC_VOLD,  XXXXXXX,  KC_VOLU,  XXXXXXX,  /**/  KC_F10,  KC_F1,   KC_F2,   KC_F3,   KC_NO,
+                                  KC_TRNS,  XXXXXXX,  /**/  XXXXXXX, KC_TRNS
   ),
   [_NUM] = LAYOUT(
-    MO(_RES), KC_NO,    KC_NO,    KC_NO,    KC_NO,    /**/  KC_PEQL, KC_7,    KC_8,    KC_9,    KC_PPLS,
+    MO(_RES), XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  /**/  KC_PEQL, KC_7,    KC_8,    KC_9,    KC_PPLS,
     KC_LALT,  KC_LGUI,  KC_LSFT,  KC_LCTL,  KC_RALT,  /**/  KC_PAST, KC_4,    KC_5,    KC_6,    KC_PMNS,
-    KC_NO,    KC_APP,   KC_TAB,   KC_BSPC,  KC_ENT,   /**/  KC_0,    KC_1,    KC_2,    KC_3,    KC_PSLS,
-                                  KC_NO,    KC_TRNS,  /**/  KC_NO,   KC_NO
+    XXXXXXX,  KC_APP,   KC_TAB,   KC_BSPC,  KC_ENT,   /**/  KC_0,    KC_1,    KC_2,    KC_3,    KC_PSLS,
+                                  XXXXXXX,  KC_TRNS,  /**/  XXXXXXX, KC_NO
   ),
   [_RES] = LAYOUT(
-    KC_TRNS,  QK_BOOT,  KC_NO,    KC_NO,    KC_NO,    /**/  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    /**/  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    /**/  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-                                  KC_NO,    KC_NO,    /**/  KC_NO,   KC_NO 
+    KC_TRNS,  QK_BOOT,  XXXXXXX,  XXXXXXX,  XXXXXXX,  /**/  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_NO,
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  /**/  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_NO,
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  /**/  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_NO,
+                                  XXXXXXX,  XXXXXXX,  /**/  XXXXXXX, KC_NO 
   ),
 };
