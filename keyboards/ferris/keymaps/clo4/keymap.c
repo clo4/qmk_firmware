4d95984bfea19e1798436dee1b150919d982e432
Robert <git@clo4.net>
2023-06-06 14:15:00 +1000

    Reorder modifiers
    Previously it was difficult to use repeated shortcuts like C-d and C-p
    because the same finger had to be used for both. This meant it wasn't
    possible to hold the control key and tap the letter, instead forcing
    you to tap control then tap the letter.
    
    Using this modifier order fixes the issue by moving control to the
    pinky, allowing it to be held for C-d and C-p.

--- keyboards/ferris/keymaps/clo4/keymap.c
+++ keyboards/ferris/keymaps/clo4/keymap.c
@@ -62,25 +62,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [_EXT] = LAYOUT(
     KC_ESC,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  /**/  KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_CAPS,
-    OSM_LALT, OSM_LGUI, OSM_LSFT, OSM_LCTL, OSM_RALT, /**/  KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_DEL,
+    OSM_LCTL, OSM_LALT, OSM_LSFT, OSM_LGUI, OSM_RALT, /**/  KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_DEL,
     KC_LCTZ,  KC_LCTX,  KC_LCTC,  KC_LGUI,  KC_LCTV,  /**/  KC_ENT,  KC_BSPC, KC_TAB,  KC_APP,  KC_PSCR,
                                   _______,  XXXXXXX,  /**/  KC_ENT,  MO(_FUN) 
   ),
   [_SYM] = LAYOUT(
     KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  /**/  KC_EQL,  KC_GRV,  KC_COLN, KC_SCLN, KC_PLUS,
-    KC_LALT,  KC_LGUI,  KC_LSFT,  KC_LCTL,  KC_CIRC,  /**/  KC_ASTR, KC_LPRN, KC_LCBR, KC_LBRC, KC_MINS,
+    KC_LCTL,  KC_LALT,  KC_LSFT,  KC_LGUI,  KC_CIRC,  /**/  KC_ASTR, KC_LPRN, KC_LCBR, KC_LBRC, KC_MINS,
     XXXXXXX,  XXXXXXX,  KC_BSLS,  KC_PIPE,  KC_AMPR,  /**/  KC_TILD, KC_RPRN, KC_RCBR, KC_RBRC, KC_UNDS,
                                   MO(_NUM), XXXXXXX,  /**/  XXXXXXX, _______
   ),
   [_FUN] = LAYOUT(
     KC_MSTP,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_BRIU,  /**/  KC_F12,  KC_F7,   KC_F8,   KC_F9,   XXXXXXX,
-    KC_LALT,  KC_LGUI,  KC_LSFT,  KC_LCTL,  KC_BRID,  /**/  KC_F11,  KC_F4,   KC_F5,   KC_F6,   XXXXXXX,
+    KC_LCTL,  KC_LALT,  KC_LSFT,  KC_LGUI,  KC_BRID,  /**/  KC_F11,  KC_F4,   KC_F5,   KC_F6,   XXXXXXX,
     KC_MUTE,  KC_VOLD,  XXXXXXX,  KC_VOLU,  XXXXXXX,  /**/  KC_F10,  KC_F1,   KC_F2,   KC_F3,   XXXXXXX,
                                   _______,  XXXXXXX,  /**/  XXXXXXX, _______
   ),
   [_NUM] = LAYOUT(
     MO(_RES), XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  /**/  KC_PEQL, KC_7,    KC_8,    KC_9,    KC_PPLS,
-    KC_LALT,  KC_LGUI,  KC_LSFT,  KC_LCTL,  KC_RALT,  /**/  KC_PAST, KC_4,    KC_5,    KC_6,    KC_PMNS,
+    KC_LCTL,  KC_LALT,  KC_LSFT,  KC_LGUI,  KC_RALT,  /**/  KC_PAST, KC_4,    KC_5,    KC_6,    KC_PMNS,
     XXXXXXX,  KC_APP,   KC_TAB,   KC_BSPC,  KC_ENT,   /**/  KC_0,    KC_1,    KC_2,    KC_3,    KC_PSLS,
                                   _______,  XXXXXXX,  /**/  XXXXXXX, _______
   ),
