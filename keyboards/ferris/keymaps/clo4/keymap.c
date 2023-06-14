14245000d1de43d97d44e26a5f8c268683345f0b
clo4 <git@clo4.net>
2023-06-14 22:16:27 +1000

    Swap space and shift

--- keyboards/ferris/keymaps/clo4/keymap.c
+++ keyboards/ferris/keymaps/clo4/keymap.c
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,     /**/  KC_J,    KC_L,    KC_U,    KC_Y,    KC_QUOT,
     KC_A,     KC_R,     KC_S,     KC_T,     KC_G,     /**/  KC_M,    KC_N,    KC_E,    KC_I,    KC_O,
     KC_Z,     KC_X,     KC_C,     KC_D,     KC_V,     /**/  KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH,
-                                  MO(_EXT), KC_LSFT,  /**/  KC_SPC,  MO(_SYM)
+                                  MO(_EXT), KC_SPC,   /**/  KC_LSFT, MO(_SYM)
   ),
   // I've added a key for C-b to make tmux easier to use.
   // Custom: QK_REP, QK_AREP, KC_LCTB
