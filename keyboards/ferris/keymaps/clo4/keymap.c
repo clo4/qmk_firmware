dad486ced61dc351067499dce8ead449422314ab
clo4 <git@clo4.net>
2024-04-23 03:25:14 +1000

    Improved layout with changes after iteration

--- keyboards/ferris/keymaps/clo4/keymap.c
+++ keyboards/ferris/keymaps/clo4/keymap.c
@@ -1,57 +1,25 @@
 #include QMK_KEYBOARD_H
 
 enum layers {
-  _COLEMAK_DH,
-  _EXT,
-  _SYM,
-  _FUN,
-  _NUM,
-  _RES,
+  _BASE,
+  _LOWER,
+  _RAISE,
+  _ADJUST,
+  _RESET,
 };
 
-// The defines are used to keep everything neatly aligned by columns
-#define OSM_LALT OSM(MOD_LALT)
-#define OSM_LGUI OSM(MOD_LGUI)
-#define OSM_LSFT OSM(MOD_LSFT)
-#define OSM_LCTL OSM(MOD_LCTL)
-#define OSM_RALT OSM(MOD_RALT)
-#define KC_LCTZ LCTL(KC_Z)
-#define KC_LCTX LCTL(KC_X)
-#define KC_LCTC LCTL(KC_C)
-#define KC_LCTV LCTL(KC_V)
-#define KC_LCTB LCTL(KC_B)
+const uint16_t PROGMEM combo_scln[] = {KC_DOT, KC_SLSH, COMBO_END};
+const uint16_t PROGMEM combo_mins[] = {KC_COMM, KC_DOT, COMBO_END};
+const uint16_t PROGMEM combo_coln[] = {KC_H, KC_COMM, COMBO_END};
 
-enum custom_keycodes {
-  /* Sends the string ` = ` */
-  STR_EQ1 = SAFE_RANGE,
-  /* Sends the string ` == ` */
-  STR_EQ2,
-  /* Sends the string ` === ` */
-  STR_EQ3,
+combo_t key_combos[] = {
+    COMBO(combo_scln, KC_SCLN),
+    COMBO(combo_mins, KC_MINS),
+    COMBO(combo_coln, KC_COLN),
 };
-
-// This layout is based on Seniply:
-// https://stevep99.github.io/seniply/
-//
-// The only major change from the stock layout is how the layers are ordered.
-// There are 4 thumb keys on the Ferris Sweep: A, B # C, D (where # is the split)
-// B is shift and C is space. These never change in any layer.
-// Hold A to enter EXT. Hold A->D to enter FUN.
-// Hold D to enter SYM. Hold D->A to enter NUM.
-//
-// There is also a RES layer to type QK_BOOT. This is accessed by entering the 
-// NUM layer, holding the top-right key, then tapping the key in the W position.
-//
-// There are other minor changes that are documented in comments above each layer.
-
-// Styleguide:
-// - Limit key names to at most 8 chars
-// - Align by columns (see below)
-// - Keep two spaces either side of the split comment /**/
-// - Use XXXXXXX and _______ instead of the corresponding KC_*
 
 // To align by cols, type this sequence when your cursor is over the { below
-// mi{sLAY<enter>j3xS,<ret>_<A-;>&
+// mi{sLAY<ret>j3xS,<ret>_<A-;>&
 // Broken down:
 // - mi{ selects inside the array
 // - sLAY<ret> selects the LAYOUT macros (we don't need the entire macro, just a cursor on it)
@@ -63,80 +31,34 @@ enum custom_keycodes {
 // - & aligns the selections to columns
 // You must align the thumb row manually.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_COLEMAK_DH] = LAYOUT(
-    KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,     /**/  KC_J,    KC_L,    KC_U,    KC_Y,    KC_QUOT,
-    KC_A,     KC_R,     KC_S,     KC_T,     KC_G,     /**/  KC_M,    KC_N,    KC_E,    KC_I,    KC_O,
-    KC_Z,     KC_X,     KC_C,     KC_D,     KC_V,     /**/  KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH,
-                                  MO(_EXT), KC_SPC,   /**/  KC_LSFT, MO(_SYM)
+  [_BASE] = LAYOUT(
+    KC_Q,       KC_W,         KC_F,         KC_P,      KC_B,      /**/  KC_J,    KC_L,    KC_U,       KC_Y,    KC_QUOT,
+    KC_A,       KC_R,         KC_S,         KC_T,      KC_G,      /**/  KC_M,    KC_N,    KC_E,       KC_I,    KC_O,
+    KC_Z,       KC_X,         KC_C,         KC_D,      KC_V,      /**/  KC_K,    KC_H,    KC_COMM,    KC_DOT,  KC_SLSH,
+                                            TL_LOWR,   KC_SPC,    /**/  KC_LSFT, TL_UPPR
   ),
-  // I've added a key for C-b to make tmux easier to use.
-  // Custom: QK_REP, QK_AREP, KC_LCTB
-  [_EXT] = LAYOUT(
-    KC_ESC,   XXXXXXX,  QK_REP,   QK_AREP,  KC_LCTB,  /**/  KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_CAPS,
-    OSM_LCTL, OSM_LALT, OSM_LSFT, OSM_LGUI, OSM_RALT, /**/  KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_DEL,
-    KC_LCTZ,  KC_LCTX,  KC_LCTC,  KC_LGUI,  KC_LCTV,  /**/  KC_ENT,  KC_BSPC, KC_TAB,  KC_APP,  KC_PSCR,
-                                  _______,  XXXXXXX,  /**/  KC_ENT,  MO(_FUN) 
+  [_LOWER] = LAYOUT(
+    KC_ESC,     S(G(KC_SPC)), S(C(KC_TAB)), C(KC_TAB), XXXXXXX,   /**/  XXXXXXX, KC_MPRV, KC_MPLY,    KC_MNXT, KC_CAPS,
+    KC_LSFT,    KC_LALT,      KC_LCTL,      KC_LGUI,   G(KC_SPC), /**/  KC_LEFT, KC_DOWN, KC_UP,      KC_RGHT, KC_ENT,
+    MO(_RESET), XXXXXXX,      KC_PGUP,      KC_PGDN,   XXXXXXX,   /**/  XXXXXXX, KC_BSPC, A(KC_BSPC), KC_TAB,  KC_DEL,
+                                            _______,   _______,   /**/  _______, _______
   ),
-  // Custom: STR_EQL in bottom left, KC_SPC on left thumb
-  [_SYM] = LAYOUT(
-    KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  /**/  KC_PLUS, KC_GRV,  KC_COLN, KC_SCLN, KC_EQL,
-    KC_LCTL,  KC_LALT,  KC_LSFT,  KC_LGUI,  KC_CIRC,  /**/  KC_ASTR, KC_LPRN, KC_LCBR, KC_LBRC, KC_MINS,
-    STR_EQ2,  STR_EQ3,  KC_BSLS,  KC_PIPE,  KC_AMPR,  /**/  KC_TILD, KC_RPRN, KC_RCBR, KC_RBRC, KC_UNDS,
-                                  MO(_NUM), STR_EQ1,  /**/  XXXXXXX, _______
+  [_RAISE] = LAYOUT(
+    KC_GRV,     KC_LBRC,      KC_RBRC,      KC_TILD,   KC_PERC,   /**/  KC_ASTR, KC_HASH, KC_LABK,    KC_RABK, KC_DQUO,
+    KC_LPRN,    KC_LCBR,      KC_RCBR,      KC_RPRN,   KC_AMPR,   /**/  KC_DLR,  KC_RGUI, KC_RCTL,    KC_RALT, KC_RSFT,
+    KC_SCLN,    KC_BSLS,      KC_EQL,       KC_MINS,   KC_PLUS,   /**/  KC_CIRC, KC_PIPE, KC_UNDS,    KC_AT,   KC_EXLM,
+                                            _______,   _______,   /**/  _______, _______
   ),
-  [_FUN] = LAYOUT(
-    KC_MSTP,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_BRIU,  /**/  KC_F12,  KC_F7,   KC_F8,   KC_F9,   XXXXXXX,
-    KC_LCTL,  KC_LALT,  KC_LSFT,  KC_LGUI,  KC_BRID,  /**/  KC_F11,  KC_F4,   KC_F5,   KC_F6,   XXXXXXX,
-    KC_MUTE,  KC_VOLD,  XXXXXXX,  KC_VOLU,  XXXXXXX,  /**/  KC_F10,  KC_F1,   KC_F2,   KC_F3,   XXXXXXX,
-                                  _______,  XXXXXXX,  /**/  XXXXXXX, _______
+  [_ADJUST] = LAYOUT(
+    KC_F1,      KC_F2,        KC_F3,        KC_F4,     KC_F5,     /**/  KC_F6,   KC_F7,   KC_F8,      KC_F9,   KC_F10, 
+    KC_1,       KC_2,         KC_3,         KC_4,      KC_5,      /**/  KC_6,    KC_7,    KC_8,       KC_9,    KC_0, 
+    KC_LSFT,    KC_LALT,      KC_LCTL,      KC_LGUI,   KC_F11,    /**/  KC_F12,  KC_RGUI, KC_RCTL,    KC_RALT, KC_RSFT,
+                                            _______,   _______,   /**/  _______, _______
   ),
-  // Number pad:
-  //  = 7 8 9 +
-  //  * 4 5 6 -
-  //  0 1 2 3 /
-  // Custom: KC_DOT on bottom left
-  [_NUM] = LAYOUT(
-    MO(_RES), STR_EQ3,  STR_EQ1,  STR_EQ2,  XXXXXXX,  /**/  KC_PLUS, KC_7,    KC_8,    KC_9,    KC_EQL,
-    KC_LCTL,  KC_LALT,  KC_LSFT,  KC_LGUI,  KC_RALT,  /**/  KC_ASTR, KC_4,    KC_5,    KC_6,    KC_MINS,
-    KC_DOT,   KC_APP,   KC_TAB,   KC_BSPC,  KC_ENT,   /**/  KC_0,    KC_1,    KC_2,    KC_3,    KC_SLSH,
-                                  _______,  XXXXXXX,  /**/  KC_SPC,  _______
-  ),
-  [_RES] = LAYOUT(
-    _______,  XXXXXXX,  XXXXXXX,  QK_BOOT,  XXXXXXX,  /**/  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  /**/  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  /**/  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                                  XXXXXXX,  XXXXXXX,  /**/  XXXXXXX, XXXXXXX 
+  [_RESET] = LAYOUT(
+    XXXXXXX,    XXXXXXX,      XXXXXXX,      QK_BOOT,   XXXXXXX,   /**/  XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX,
+    XXXXXXX,    XXXXXXX,      XXXXXXX,      XXXXXXX,   XXXXXXX,   /**/  XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX,
+    _______,    XXXXXXX,      XXXXXXX,      XXXXXXX,   XXXXXXX,   /**/  XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX,
+                                            XXXXXXX,   XXXXXXX,   /**/  XXXXXXX, XXXXXXX 
   ),
 };
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  if (record->event.pressed) {
-    switch (keycode) {
-      case STR_EQ1:
-        SEND_STRING(" = ");
-        return false;
-
-      case STR_EQ2:
-        SEND_STRING(" == ");
-        return false;
-
-      case STR_EQ3:
-        SEND_STRING(" === ");
-        return false;
-    }
-  }
-  return true;
-}
-
-uint16_t get_alt_repeat_key_keycode_user(uint16_t keycode, uint8_t mods) {
-  bool shifted = (mods & MOD_MASK_SHIFT);
-  switch (keycode) {
-    case KC_TAB:
-      if (shifted) {
-        return KC_TAB;
-      } else {
-        return S(KC_TAB);
-      }
-  }
-  return KC_TRNS;
-}
--- keyboards/ferris/keymaps/clo4/rules.mk
+++ keyboards/ferris/keymaps/clo4/rules.mk
@@ -1,4 +1,6 @@
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 # TAP_DANCE_ENABLE = yes
 BOOTLOADER = atmel-dfu		# Using Elite C
-REPEAT_KEY_ENABLE = yes
+# REPEAT_KEY_ENABLE = yes
+TRI_LAYER_ENABLE = yes
+COMBO_ENABLE = yes
